7f3185584f9e96a7ec68fd65835d363f60780788
Hieu Liem <hieuliem33@gmail.com>
2023-09-24 21:50:12 +0700

    setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: process.env.NEXT_PUBLIC_IS_EXPORT === 'true' ? 'export' : undefined,
+  distDir: process.env.NEXT_PUBLIC_IS_EXPORT === 'true' ? 'wwwroot' : undefined,
+}
 
 module.exports = nextConfig
